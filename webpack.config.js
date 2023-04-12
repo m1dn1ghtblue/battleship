d11fa0d0c76ae3637e535c1e89aba10dda8b1635
Ilya <kotyrov.1310@gmail.com>
2023-04-12 21:39:40 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/scripts/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index.bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+};
