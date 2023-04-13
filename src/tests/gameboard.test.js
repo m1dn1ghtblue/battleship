a68215f4d306599bf76aac2aaa091dac613ced91
Ilya <kotyrov.1310@gmail.com>
2023-04-13 19:36:41 +0200

    add test

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -19,22 +19,25 @@ describe('Gameboard factory function', () => {
 
 	test('Gameboard object should have placeShip method that creates ship with given length on given coordinates', () => {
 		const gameboard = new Gameboard();
-		gameboard.placeShip([0, 0], 1, 'horizontal');
-		gameboard.placeShip([2, 2], 4, 'horizontal');
+		gameboard.placeShip([7, 9], 1, 'horizontal');
+		gameboard.placeShip([2, 3], 4, 'horizontal');
 		gameboard.placeShip([4, 4], 3, 'vertical');
+		gameboard.placeShip([0, 0], 2, 'vertical');
 
-		expect(gameboard.getCell([0, 0]).ship).not.toBeNull();
+		expect(gameboard.getCell([7, 9]).ship).not.toBeNull();
 
-		expect(gameboard.getCell([2, 2]).ship).not.toBeNull();
 		expect(gameboard.getCell([2, 3]).ship).not.toBeNull();
 		expect(gameboard.getCell([2, 4]).ship).not.toBeNull();
 		expect(gameboard.getCell([2, 5]).ship).not.toBeNull();
-		expect(gameboard.getCell([2, 2]).ship).toBe(gameboard.getCell([2, 5]).ship);
+		expect(gameboard.getCell([2, 6]).ship).not.toBeNull();
+		expect(gameboard.getCell([2, 3]).ship).toBe(gameboard.getCell([2, 6]).ship);
 
 		expect(gameboard.getCell([4, 4]).ship).not.toBeNull();
 		expect(gameboard.getCell([5, 4]).ship).not.toBeNull();
 		expect(gameboard.getCell([6, 4]).ship).not.toBeNull();
 		expect(gameboard.getCell([4, 4]).ship).toBe(gameboard.getCell([6, 4]).ship);
+
+		expect(gameboard.getCell([0, 0]).ship).toBe(gameboard.getCell([1, 0]).ship);
 	});
 
 	test('Gameboard object should not allow to place ships in adjacent cells', () => {
@@ -77,10 +80,19 @@ describe('Gameboard factory function', () => {
 	test('Gameboard should have method to report whether all ship cells have been hit', () => {
 		const gameboard = new Gameboard();
 		gameboard.placeShip([5, 5], 1, 'horizontal');
+		gameboard.placeShip([0, 0], 2, 'vertical');
 
 		expect(gameboard.isGameOver).toBe(false);
 
 		gameboard.receiveAttack([5, 5]);
+
+		expect(gameboard.isGameOver).toBe(false);
+
+		gameboard.receiveAttack([0, 0]);
+
+		expect(gameboard.isGameOver).toBe(false);
+
+		gameboard.receiveAttack([1, 0]);
 
 		expect(gameboard.isGameOver).toBe(true);
 	});
