4cbd13a94b25d07dd478904277cd73d5bf031246
Ilya <kotyrov.1310@gmail.com>
2023-04-14 11:01:46 +0200

    implement game factory

---
+++ src/scripts/game/game.js
@@ -0,0 +1,40 @@
+export default function Game(player1, player2) {
+	const _playerOne = player1;
+	const _playerTwo = player2;
+	let _playerOneTurn = true;
+
+	function takeTurn(coords) {
+		let switchPlayers;
+
+		try {
+			const isHit = (_playerOneTurn ? _playerTwo : _playerOne).gameboard.receiveAttack(coords);
+			switchPlayers = !isHit;
+		} catch (error) {
+			switchPlayers = false;
+		} finally {
+			if (switchPlayers) {
+				_playerOneTurn = !_playerOneTurn;
+			}
+		}
+	}
+
+	return {
+		get playerOne() {
+			return _playerOne;
+		},
+
+		get playerTwo() {
+			return _playerTwo;
+		},
+
+		get activePlayer() {
+			return _playerOneTurn ? _playerOne : _playerTwo;
+		},
+
+		get isGameOver() {
+			return _playerOne.gameboard.isGameOver || _playerTwo.gameboard.isGameOver;
+		},
+
+		takeTurn,
+	};
+}
