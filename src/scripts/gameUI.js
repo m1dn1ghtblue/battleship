f5d16651f6fcc06aa5c21aa1330560a18db6a207
Ilya <kotyrov.1310@gmail.com>
2023-04-14 13:58:51 +0200

    implement turns on grid cell click

--- src/scripts/gameUI.js
+++ src/scripts/gameUI.js
@@ -1,16 +1,70 @@
 'use strict';
 
-import Game from './game/game.js';
-import Player from './game/player.js';
 import '../styles/gameboard.scss';
 
 export default function GameUI(game) {
-	const gameState = game;
+	const _gameState = game;
 
-	return {};
+	const _playerOneGrid = makeGameboardGrid(_gameState.playerOne.gameboard, _takePlayerTwoTurn);
+	const _playerTwoGrid = makeGameboardGrid(_gameState.playerTwo.gameboard, _takePlayerOneTurn);
+
+	const _playerOneGameboard = makeGameboard(_playerOneGrid);
+	const _playerTwoGameboard = makeGameboard(_playerTwoGrid);
+
+	function _takePlayerOneTurn(row, col) {
+		if (_gameState.activePlayer === _gameState.playerOne) {
+			try {
+				_gameState.takeTurn([row, col]);
+				_updateGrid(_playerTwoGrid, _gameState.playerTwo.gameboard);
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	}
+
+	function _takePlayerTwoTurn(row, col) {
+		if (_gameState.activePlayer === _gameState.playerTwo) {
+			try {
+				_gameState.takeTurn([row, col]);
+				_updateGrid(_playerOneGrid, _gameState.playerOne.gameboard);
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	}
+
+	function _updateGrid(grid, gameboard) {
+		const cells = grid.childNodes;
+		for (let row = 0; row < 10; ++row) {
+			for (let col = 0; col < 10; ++col) {
+				const cell = cells[row * 10 + col];
+				if (gameboard.getCell([row, col]).isHit) {
+					cell.classList.add('hit');
+				}
+			}
+		}
+	}
+
+	return {
+		get playerOneGameboard() {
+			return _playerOneGameboard;
+		},
+
+		get playerTwoGameboard() {
+			return _playerTwoGameboard;
+		},
+
+		get playerOneName() {
+			return _gameState.playerOne.name;
+		},
+
+		get playerTwoName() {
+			return _gameState.playerTwo.name;
+		},
+	};
 }
 
-function makeGameboard(playerGameboard) {
+function makeGameboard(grid) {
 	const gameboard = document.createElement('div');
 	gameboard.classList.add('gameboard');
 
@@ -19,25 +73,25 @@ function makeGameboard(playerGameboard) {
 		const markupElement = document.createElement('span');
 		markupElement.textContent = String.fromCharCode(aCharCode + col);
 		markupElement.classList.add('gameboard-markup');
-		markupElement.style.gridRow('1');
-		markupElement.style.gridColumn(`${col + 2}`);
+		markupElement.style.gridRow = 1;
+		markupElement.style.gridColumn = `${col + 2}`;
 		gameboard.appendChild(markupElement);
 	}
 	for (let row = 0; row < 10; ++row) {
 		const markupElement = document.createElement('span');
 		markupElement.textContent = `${row + 1}`;
 		markupElement.classList.add('gameboard-markup');
-		markupElement.style.gridColumn('1');
-		markupElement.style.gridRow(`${row + 2}`);
+		markupElement.style.gridColumn = 1;
+		markupElement.style.gridRow = `${row + 2}`;
 		gameboard.appendChild(markupElement);
 	}
 
-	gameboard.appendChild(makeGameboardGrid(playerGameboard));
+	gameboard.appendChild(grid);
 
 	return gameboard;
 }
 
-function makeGameboardGrid(playerGameboard) {
+function makeGameboardGrid(playerGameboard, onclickCallback) {
 	const grid = document.createElement('div');
 	grid.classList.add('gameboard-grid');
 
@@ -45,11 +99,16 @@ function makeGameboardGrid(playerGameboard) {
 		for (let col = 0; col < 10; ++col) {
 			const cell = document.createElement('div');
 			cell.classList.add('gameboard-grid-cell');
-			if (playerGameboard.getCell([col, row]).ship != null) {
+			if (playerGameboard.getCell([row, col]).ship != null) {
 				cell.classList.add('ship');
 			}
-			cell.style.gridRow = row;
-			cell.style.gridColumn = col;
+			cell.style.gridRow = row + 1;
+			cell.style.gridColumn = col + 1;
+
+			cell.addEventListener('click', () => {
+				onclickCallback(row, col);
+			});
+
 			grid.appendChild(cell);
 		}
 	}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,20 @@
 'use strict';
 
 import '../styles/index.scss';
+import GameUI from './gameUI.js';
+import Player from './game/player.js';
+import Game from './game/game.js';
+
+const playerOneGameboardContainer = document.getElementById('player-one-gameboard-container');
+const playerTwoGameboardContainer = document.getElementById('player-two-gameboard-container');
+
+const player1 = new Player('Alice');
+const player2 = new Player('Bob');
+player1.gameboard.placeShip([3, 3], 3, 'horizontal');
+player2.gameboard.placeShip([2, 4], 4, 'vertical');
+
+const game = new Game(player1, player2);
+const gameUI = new GameUI(game);
+
+playerOneGameboardContainer.appendChild(gameUI.playerOneGameboard);
+playerTwoGameboardContainer.appendChild(gameUI.playerTwoGameboard);
