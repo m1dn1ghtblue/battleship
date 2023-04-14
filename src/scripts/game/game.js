fb8693638877cf08bacfbbce974ee677097fa9ed
Ilya <kotyrov.1310@gmail.com>
2023-04-14 11:19:26 +0200

    Revert "rework game factory to pass new tests"
    This reverts commit 1133a01ad08d8dfd810a8576a506469a62709f4b.

--- src/scripts/game/game.js
+++ src/scripts/game/game.js
@@ -3,25 +3,11 @@ export default function Game(player1, player2) {
 	const _playerTwo = player2;
 	let _playerOneTurn = true;
 
-	function takePlayerOneTurn(coords) {
-		if (!_playerOneTurn) {
-			throw Error('Cannot take player one turn while it is not their turn');
-		}
-		_takeTurn(coords, _playerTwo.gameboard);
-	}
-
-	function takePlayerTwoTurn(coords) {
-		if (_playerOneTurn) {
-			throw Error('Cannot take player two turn while it is not their turn');
-		}
-		_takeTurn(coords, _playerOne.gameboard);
-	}
-
-	function _takeTurn(coords, gameboard) {
+	function takeTurn(coords) {
 		let switchPlayers;
 
 		try {
-			const isHit = gameboard.receiveAttack(coords);
+			const isHit = (_playerOneTurn ? _playerTwo : _playerOne).gameboard.receiveAttack(coords);
 			switchPlayers = !isHit;
 		} catch (error) {
 			switchPlayers = false;
@@ -33,15 +19,22 @@ export default function Game(player1, player2) {
 	}
 
 	return {
-		get isPlayerOneTurn() {
-			return _playerOneTurn;
+		get playerOne() {
+			return _playerOne;
+		},
+
+		get playerTwo() {
+			return _playerTwo;
+		},
+
+		get activePlayer() {
+			return _playerOneTurn ? _playerOne : _playerTwo;
 		},
 
 		get isGameOver() {
 			return _playerOne.gameboard.isGameOver || _playerTwo.gameboard.isGameOver;
 		},
 
-		takePlayerOneTurn,
-		takePlayerTwoTurn,
+		takeTurn,
 	};
 }
