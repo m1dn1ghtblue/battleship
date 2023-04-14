1133a01ad08d8dfd810a8576a506469a62709f4b
Ilya <kotyrov.1310@gmail.com>
2023-04-14 11:19:26 +0200

    rework game factory to pass new tests

--- src/scripts/game/game.js
+++ src/scripts/game/game.js
@@ -3,11 +3,25 @@ export default function Game(player1, player2) {
 	const _playerTwo = player2;
 	let _playerOneTurn = true;
 
-	function takeTurn(coords) {
+	function takePlayerOneTurn(coords) {
+		if (!_playerOneTurn) {
+			throw Error('Cannot take player one turn while it is not their turn');
+		}
+		_takeTurn(coords, _playerTwo.gameboard);
+	}
+
+	function takePlayerTwoTurn(coords) {
+		if (_playerOneTurn) {
+			throw Error('Cannot take player two turn while it is not their turn');
+		}
+		_takeTurn(coords, _playerOne.gameboard);
+	}
+
+	function _takeTurn(coords, gameboard) {
 		let switchPlayers;
 
 		try {
-			const isHit = (_playerOneTurn ? _playerTwo : _playerOne).gameboard.receiveAttack(coords);
+			const isHit = gameboard.receiveAttack(coords);
 			switchPlayers = !isHit;
 		} catch (error) {
 			switchPlayers = false;
@@ -19,22 +33,15 @@ export default function Game(player1, player2) {
 	}
 
 	return {
-		get playerOne() {
-			return _playerOne;
-		},
-
-		get playerTwo() {
-			return _playerTwo;
-		},
-
-		get activePlayer() {
-			return _playerOneTurn ? _playerOne : _playerTwo;
+		get isPlayerOneTurn() {
+			return _playerOneTurn;
 		},
 
 		get isGameOver() {
 			return _playerOne.gameboard.isGameOver || _playerTwo.gameboard.isGameOver;
 		},
 
-		takeTurn,
+		takePlayerOneTurn,
+		takePlayerTwoTurn,
 	};
 }
